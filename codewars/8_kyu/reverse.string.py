1b801cb689a089cff9dfdeaa368cfe7f71d1452d
Kurapickle <99564148+Kurapickle@users.noreply.github.com>
2022-04-30 17:37:27 +0100

    Add files via upload

---
+++ codewars/8_kyu/reverse.string.py
@@ -0,0 +1,7 @@
+#   https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+'''
+Complete the solution so that it reverses the string passed into it.
+'''
+def solution(string):
+    txt = string[::-1]
+    return txt
