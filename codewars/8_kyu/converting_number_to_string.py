58e04c8f35b5fce9398fc6731770827473d22d37
Kurapickle <99564148+Kurapickle@users.noreply.github.com>
2022-05-06 21:59:43 +0100

    Add files via upload

---
+++ codewars/8_kyu/converting_number_to_string.py
@@ -0,0 +1,7 @@
+#   https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/python
+'''
+We need a function that can transform a number into a string.
+'''
+def number_to_string(num):
+    num = str(num)
+    return num
